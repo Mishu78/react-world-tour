efb35e9c080f532b3e4dd5f54bddcbfb28f4800b
programming hero <meherun2002@gmail.com>
2026-01-30 22:09:09 +0600

    mark visited button added conditional css and style added

--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,19 +1,22 @@
 import './country.css'
 import { useState } from 'react';
-const Country = ({country}) => {
+const Country = ({country,handleVisitedCountry}) => {
     const {name,flags,population,area,cca3}=country;
     const[visited,setVisited]=useState(false);
     const handleVisited=()=>{
         setVisited(!visited);
     }
+    console.log(handleVisitedCountry)
     return (
-        <div className="country">
-            <h3>{name.common}</h3>
+        <div className={`country ${visited ? 'visited':'non-visited'}`}>
+            <h3 style={{color:visited ? 'purple': 'white'}}>{name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population:{population}</p>
             <p>area:{area}</p>
             <p><small>code:{cca3}</small></p>
-            <button onClick={handleVisited}>{visited ? 'Visited': 'Going'}</button>
+            <button>Mark visited</button>
+            <br />
+            <button onClick={handleVisited}>{visited ? 'visited': 'Going'}</button>
              {visited ? 'I have visited this country':'I want to visit'}
         </div>
     );
